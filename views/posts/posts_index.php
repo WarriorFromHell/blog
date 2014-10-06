542776e50424a6aa11cf2e7d1666b821aae521ff
Markus <markus.joemets@khk.ee>
2014-10-06 16:31:55 +0300

    Fix tags

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -5,6 +5,11 @@
         <div>
 
         </div>
+        <div class="pull-right">
+            <?foreach($tags[$post['post_id']] as $tag):?>
+                <a href="#"><span class="label" style="background-color: #5bc0de"><?=$tag?></span></a><? endforeach?>
+
+        </div>
     </div>
 <?endforeach?>
 <?foreach ($tags[$post['$post_id']] as $tag):?>
