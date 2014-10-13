82425c0ca3776f56eee1e5778a285a45f706b9dc
Markus <markus.joemets@khk.ee>
2014-10-13 10:38:45 +0300

    date/time/post

--- views/posts/posts_view.php
+++ views/posts/posts_view.php
@@ -3,7 +3,7 @@
    <h1> <?=$post['post_subject']?> </h1>
     <p> <?=$post['post_text']?> </p>
     <div>
-        <span class="badge badge-success">Posted 2012-08-02 20:47:04</span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
+        <span class="badge badge-success"><?=$today = date("F j, Y, g:i a"); ?></span><div class="pull-right"><span class="label">alice</span> <span class="label">story</span> <span class="label">blog</span> <span class="label">personal</span></div>
     </div>
 </div>
 </div>
